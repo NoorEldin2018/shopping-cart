48dd52c826a443dc67508e2eba71864cdf46ef5b
Noor Eldin Essam <n.essam@orangestudio.com>
2021-01-12 19:09:17 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './products/products.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"products",component:ProductsComponent},
+  {path:"orders",component:MyOrdersComponent},
+  {path:"admin/orders",component:AdminOrdersComponent},
+  {path:"admin/products",component:AdminProductsComponent},
+  {path:"shopping-cart",component:ShoppingCartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
